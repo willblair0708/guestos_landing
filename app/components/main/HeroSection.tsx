7f38d1a10132c7e4d5d625431563406d834e1276
willblair0708 <85643015+willblair0708@users.noreply.github.com>
2024-11-15 17:08:06 -0500

    main hero

--- app/components/main/HeroSection.tsx
+++ app/components/main/HeroSection.tsx
@@ -96,8 +96,8 @@ const Background = ({ isMobile }: { isMobile?: boolean }) => (
     <Image
       src={
         isMobile
-          ? '/assets/main/main_hero_poster.webp'
-          : '/assets/main/main_hero_poster.webp'
+          ? '/assets/main/main_hero_poster.jpg'
+          : '/assets/main/main_hero_poster.jpg'
       }
       alt='Hero background'
       fill
