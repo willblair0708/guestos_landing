44d1e4dd6044f3b86d300c9c757951503b6fb847
willblair0708 <85643015+willblair0708@users.noreply.github.com>
2024-12-08 18:53:45 -0500

    fix

--- app/components/offer/HeroSection.tsx
+++ app/components/offer/HeroSection.tsx
@@ -388,7 +388,7 @@ export default function HeroSection({
                   </div>
                 </div>
 
-                <PlatformPreview />
+                {/* <PlatformPreview /> */}
               </div>
             </div>
 
