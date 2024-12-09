5ff00e44f4dd9372dd1af9a3260a31d7fc9c052e
willblair0708 <85643015+willblair0708@users.noreply.github.com>
2024-12-09 10:51:33 -0500

    fix

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -2,7 +2,7 @@ import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!, {
   apiVersion: '2024-11-20.acacia',
 });
 
