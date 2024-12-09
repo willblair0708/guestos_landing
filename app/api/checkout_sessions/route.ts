63b33bd3b98dcbcc9a1eb7d329c64c858f53cd52
willblair0708 <85643015+willblair0708@users.noreply.github.com>
2024-12-09 10:42:01 -0500

    fix

--- app/api/checkout_sessions/route.ts
+++ app/api/checkout_sessions/route.ts
@@ -6,7 +6,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16'
+  apiVersion: '2024-11-20.acacia'
 });
 
 export async function POST(req: NextRequest) {
