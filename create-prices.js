f1e78001098052412071259a1343f481c8a974a8
willblair0708 <85643015+willblair0708@users.noreply.github.com>
2025-02-16 17:01:10 +0100

    key

--- create-prices.js
+++ create-prices.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 async function createProductsAndPrices() {
   try {
